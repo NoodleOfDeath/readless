e285ea3d5e75bf6e2a19a84a264b9e8033e28473
NoodleOfDeath <thom@noodleofdeath.com>
2023-02-28 15:39:34 -0500

    finally fucking fixed stupid tiktoken bs

---
+++ src/server/deps/_tiktoken_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_tiktoken_free(a: number): void;
+export function tiktoken_encode(a: number, b: number, c: number, d: number): void;
+export function tiktoken_decode(a: number, b: number, c: number, d: number): void;
+export function get_encoding(a: number): number;
+export function encoding_for_model(a: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
--- src/server/serve-cron.ts
+++ src/server/serve-cron.ts
@@ -1,2 +1,2 @@
 import 'dotenv/config';
-import './src/auto';
+import './src/cron';
