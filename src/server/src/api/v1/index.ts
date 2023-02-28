8109b2dc7e691fd2fb44cb21eef5f212949f3df3
NoodleOfDeath <thom@noodleofdeath.com>
2023-02-28 15:46:08 -0500

    add back sources route

--- src/server/src/api/v1/index.ts
+++ src/server/src/api/v1/index.ts
@@ -6,7 +6,7 @@ import { DBService } from '../../services';
 import arenaRouter from './routes/arena';
 import articleRouter from './routes/articles';
 import policyRouter from './routes/policies';
-// import sourceRouter from './routes/sources';
+import sourceRouter from './routes/sources';
 import referralRouter from './routes/referrals';
 
 const router = Router();
@@ -19,7 +19,7 @@ async function main() {
   router.use('/arena', arenaRouter);
   router.use('/articles', articleRouter);
   router.use('/policies', policyRouter);
-  // router.use('/sources', sourceRouter);
+  router.use('/sources', sourceRouter);
   router.use('/referrals', referralRouter);
 
   router.get('/healthz', (_, res) => res.send('OK'));
