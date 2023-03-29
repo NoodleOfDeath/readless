288f523a1b436d2ba2e38947926356a20a254182
NoodleOfDeath <thom@noodleofdeath.com>
2023-03-28 20:15:54 -0400

    fix(web): router

--- src/web/src/next/router.tsx
+++ src/web/src/next/router.tsx
@@ -4,28 +4,31 @@ import { useRouter as useNextRouter } from  'next/router';
 
 type RouterProps = ReturnType<typeof useNextRouter> & {
   searchParams: URLSearchParams;
-  setSearchParams: (params: Record<string, string>) => void;
+  setSearchParams: (params: Record<string, string> | ((prev?: Record<string, string>) => Record<string, string> | undefined)) => void;
 };
 
 export function useRouter(): RouterProps {
   
   const router = useNextRouter();
 
-  const setSearchParams = React.useCallback((params: Record<string, string>) => {
-    const searchParams = new URLSearchParams(router.asPath.split('?')[1]);
-    Object.keys(params).forEach((key) => {
-      searchParams.set(key, params[key]);
-    });
+  const searchParams = React.useMemo(() => new URLSearchParams(router.asPath.split('?')[1]), [router.asPath]);
+
+  const setSearchParams = React.useCallback((params?: Record<string, string> | ((prev?: Record<string, string>) => Record<string, string> | undefined)) => {
+    const prevParams = Object.fromEntries(searchParams);
+    const newParams = params instanceof Function ? params(prevParams) : params;
     router.push({
       pathname: router.pathname,
       query: router.query,
-      search: searchParams.toString(),
+      search: new URLSearchParams({
+        ...prevParams,
+        ...newParams,
+      }).toString(),
     });
-  }, [router]);
+  }, [router, searchParams]);
 
   return {
     ...router,
-    searchParams: React.useMemo(() => new URLSearchParams(router.asPath.split('?')[1]), [router.asPath]),
+    searchParams,
     setSearchParams,
   };
 }
