3a6a55820fc985fec54ed3f9d6b93ff26f2fccb6
NoodleOfDeath <thom@noodleofdeath.com>
2023-04-02 20:41:32 -0400

    fix(ap): ~web ~cron ~worker

--- src/server/src/api/v1/middleware/AuthMiddleware.ts
+++ src/server/src/api/v1/middleware/AuthMiddleware.ts
@@ -34,20 +34,15 @@ export const authMiddleware = (securityName: string, { required = false, scope =
           } catch (e) {
             if (required) {
               throw new AuthError('INVALID_CREDENTIALS');
-            } else {
-              next()
             }
           }
         } else if (required) {
           throw new AuthError('INVALID_CREDENTIALS');
-        } else {
-          next();
         }
       } else if (required) {
         throw new AuthError('MISSING_AUTHORIZATION_HEADER');
-      } else {
-        next();
       }
+      next();
     }
   } catch (e) {
     if (e instanceof AuthError) {
