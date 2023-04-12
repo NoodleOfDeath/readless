b12fce98464ef7d2ebb4a1bdc5f8e9f3cc5017bd
NoodleOfDeath <thom@noodleofdeath.com>
2023-04-12 08:21:37 -0400

    feat(api): auroemails feedback to me

--- src/server/src/services/index.ts
+++ src/server/src/services/index.ts
@@ -2,6 +2,7 @@ export * from './account';
 export * from './aws';
 export * from './db';
 export * from './google';
+export * from './mail';
 export * from './openai';
 export * from './scribe';
 export * from './spider';
