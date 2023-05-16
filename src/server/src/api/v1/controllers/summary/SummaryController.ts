b6f0e62f7978a986d2c087f9c10cc964b54d88d9
NoodleOfDeath <thom@noodleofdeath.com>
2023-05-16 13:05:35 -0400

    ~skip

--- src/server/src/api/v1/controllers/summary/SummaryController.ts
+++ src/server/src/api/v1/controllers/summary/SummaryController.ts
@@ -169,7 +169,7 @@ export class SummaryController extends BaseControllerWithPersistentStorageAccess
       replacements: {
         interval,
         limit: Number(pageSize),
-        min: Number(min < 2 ? 2 : min),
+        min: Number(min) < 2 ? 2 : Number(min),
         offset,
         order,
         type: type ?? '%',
