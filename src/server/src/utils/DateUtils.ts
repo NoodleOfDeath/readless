f8fb7444476134428dcaae4e96b6f246afe6850b
noodleofdeath <thom@noodleofdeath.com>
2023-05-24 17:32:47 -0400

    fix dates and smart search ~cron ~web

--- src/server/src/utils/DateUtils.ts
+++ src/server/src/utils/DateUtils.ts
@@ -2,7 +2,7 @@ import ms from 'ms';
 
 export const TIME_EXPR = /(\d\d?)\s*(a\.?m\.?|p\.?m\.?)|(\d\d?)[.:](\d\d?)(?:[.:](\d\d?))?(?:\s*(a\.?m\.?|p\.?m\.?))?(?:.*(?:\s*(ACDT|ACS?T|AES?T|AKDT|AKS?T|BS?T|CES?T|CDT|CS?T|EDT|ES?T|IS?T|JS?T|MDT|MSK|NZS?T|PDT|PS?T|UTC)))?/i;
 
-export const DATE_EXPR = /(\d\d?\s*(?:h|h(?:ou)?rs?|m|min(?:ute)s?))\s*ago|(?:(\d\d?)([-./])(\d\d?)\3(\d{4}|\d{2})|(\d{4})([-./])(\d\d?)\7(\d\d?)|(?:(\d\d?)(?:st|nd|rd|th)?\s*)?(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:t(?:ember)?)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)(?:\s+(\d\d?)(?:st|nd|rd|th)?)?(?:[,\s]\s*(\d{4})?))(?:.*(?:\s*(ACDT|ACS?T|AES?T|AKDT|AKS?T|BS?T|CES?T|CDT|CS?T|EDT|ES?T|IS?T|JS?T|MDT|MSK|NZS?T|PDT|PS?T|UTC)))?|(\d\d\d\d+)/i;
+export const DATE_EXPR = /(\d\d?\s*(?:h(?:rs?|ours?)?|m(?:in(?:utes?)?)?|d(?:ays?)?|w(?:ks?|eeks?)?|months?|y(?:rs?|ears?)?))\s*ago|(?:(\d\d?)([-./])(\d\d?)\3(\d{4}|\d{2})|(\d{4})([-./])(\d\d?)\7(\d\d?)|(?:(\d\d?)(?:st|nd|rd|th)?\s*)?(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:t(?:ember)?)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)(?:\s+(\d\d?)(?:st|nd|rd|th)?)?(?:[,\s]\s*(\d{4})?))(?:.*(?:\s*(ACDT|ACS?T|AES?T|AKDT|AKS?T|BS?T|CES?T|CDT|CS?T|EDT|ES?T|IS?T|JS?T|MDT|MSK|NZS?T|PDT|PS?T|UTC)))?|(\d\d\d\d+)/i;
 
 export function monthToString(month: number | string) {
   const m = parseInt(`${month}`);
@@ -12,12 +12,16 @@ export function monthToString(month: number | string) {
   return new Date(`2023-${m}-07`).toLocaleString('en-US', { month: 'long' });
 }
 
+export function dateOrUndefined(date: Date) {
+  return !Number.isNaN(date.valueOf()) && date.valueOf() > 0 ? date : undefined;
+}
+
 export function parseDate(context?: string) {
   if (!context) {
-    return new Date('invalid');
+    return undefined;
   }
-  const date = new Date(context.trim());
-  if (!Number.isNaN(date.valueOf()) && date.valueOf() > 0) {
+  const date = dateOrUndefined(new Date(context.trim()));
+  if (date) {
     return date;
   }
   const dateMatches = context.match(DATE_EXPR);
@@ -28,17 +32,21 @@ export function parseDate(context?: string) {
   let hour = 0, min = 0, sec = 0, amOrPm = '';
   let timezone = '';
   if (!dateMatches) {
-    return new Date('invalid');
+    return undefined;
   }
   const [_0, relative, month1, _3, day1, year1, year2, _7, month2, day2, day3, month3, day4, year3, tz = '', timestamp] = dateMatches;
   if (relative) {
-    return new Date(Date.now() - ms(relative.replace(/h(?:ou)?rs?/, 'h').replace(/m(?:in)?s?/, 'm')));
+    return new Date(Date.now() - ms(relative
+      .replace(/h(?:rs?|ours?)?/, 'h')
+      .replace(/m(?:in(?:utes?)?)?/, 'm')
+      .replace(/d(?:ays?)?/, 'd')
+      .replace(/w(?:ks?|eeks?)?/, 'w')
+      .replace(/months?/, 'months')
+      .replace(/y(?:rs?|ears?)?/, 'y')));
   }
   const datetime = parseInt(timestamp);
   if (!Number.isNaN(datetime)) {
-    if (!Number.isNaN(new Date(datetime)).valueOf()) {
-      return new Date(datetime);
-    }
+    return dateOrUndefined(new Date(datetime));
   }
   timezone = tz;
   year = year1 ?? year2 ?? year3 ?? String(new Date().getFullYear());
@@ -55,28 +63,28 @@ export function parseDate(context?: string) {
     }
   }
   const dateMatch = [`${month} ${day}, ${String(year).length === 2 ? `20${year}` : year} ${hour}:${min}:${sec} ${amOrPm ? amOrPm : (hour < 12) ? 'am' : 'pm'}`, timezone.replace(/^(A[CEK]|CE|NZ|[BCEIJP])T$/, ($0, $1) => `${$1}ST`)].join(' ');
-  const parsedDate = new Date(dateMatch);
+  const parsedDate = dateOrUndefined(new Date(dateMatch));
   return parsedDate;
 }
 
-export function sortDates(...dates: Date[]) {
-  return [...dates].filter((d) => !Number.isNaN(d.valueOf())).sort((a, b) => { 
+export function sortDates(...dates: (Date | undefined | null)[]) {
+  return [...dates].filter(Boolean).filter((d) => !Number.isNaN(d.valueOf())).sort((a, b) => { 
     return a.valueOf() - b.valueOf();
   });
 }
 
-export function minDate(...dates: Date[]) {
+export function minDate(...dates: (Date | undefined | null)[]) {
   const sortedDates = sortDates(...dates);
   if (sortedDates.length === 0) {
-    return new Date('invalid');
+    return undefined;
   }
   return sortedDates[0];
 }
 
-export function maxDate(...dates: Date[]) {
+export function maxDate(...dates: (Date | undefined | null)[]) {
   const sortedDates = sortDates(...dates);
   if (sortedDates.length === 0) {
-    return new Date('invalid');
+    return undefined;
   }
   return sortedDates[sortedDates.length - 1];
 }
