2b14da017a66787d549589dddfcec25e69a98cf8
noodleofdeath <thom@noodleofdeath.com>
2023-06-24 17:32:53 -0400

    fix rename

--- src/mobile/android/app/src/main/java/ai/readless/ReadLess/MainActivity.java
+++ src/mobile/android/app/src/main/java/ai/readless/ReadLess/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Read Less";
+    return "ReadLess";
   }
   
   @Override
