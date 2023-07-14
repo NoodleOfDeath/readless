eceefa48615eb4e2a94196646a18ccc7e1c19eb5
noodleofdeath <thom@noodleofdeath.com>
2023-07-14 12:40:12 -0400

    web update && v1.11.2 ipad fix

--- src/web/next.config.js
+++ src/web/next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { 
-  images: { unoptimized: true }, 
+const nextConfig = {
+  images: { unoptimized: true },
   reactStrictMode: true,
   trailingSlash: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
