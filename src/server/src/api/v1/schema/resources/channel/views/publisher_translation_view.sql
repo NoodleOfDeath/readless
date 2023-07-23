998498cbd1059fa0e812d562836ad0227cac5750
noodleofdeath <thom@noodleofdeath.com>
2023-07-23 18:36:17 -0400

    fix(~api|~worker|~caches): locale params

--- src/server/src/api/v1/schema/resources/channel/views/publisher_translation_view.sql
+++ src/server/src/api/v1/schema/resources/channel/views/publisher_translation_view.sql
@@ -1,17 +1,20 @@
 DROP VIEW IF EXISTS publisher_translation_view;
+
 CREATE OR REPLACE VIEW publisher_translation_view AS
-  SELECT
-    l.code AS locale,
-    trans."parentId",
-    COALESCE(JSON_OBJECT_AGG(
-      trans.attribute,
-      trans.value
-    ) FILTER (WHERE trans.attribute IS NOT NULL), '{}'::JSON) AS translations
-  FROM
-    locales l
-    LEFT OUTER JOIN publisher_translations trans
-      ON l.code = trans.locale
-  GROUP BY
-    l.code,
-    trans."parentId";
-SELECT * FROM publisher_translation_view LIMIT 10;
+SELECT
+  l.code AS locale,
+  trans."parentId",
+  COALESCE(JSON_OBJECT_AGG(trans.attribute, trans.value) FILTER (WHERE trans.attribute IS NOT NULL), '{}'::json) AS translations
+FROM
+  locales l
+  LEFT OUTER JOIN publisher_translations trans ON l.code = trans.locale
+GROUP BY
+  l.code,
+  trans."parentId";
+
+SELECT
+  *
+FROM
+  publisher_translation_view
+LIMIT 10;
+
