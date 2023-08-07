b9ca876a8fa356fca4d90bd5095ebf8db0e51425
NoodleOfDeath <thom@noodleofdeath.com>
2023-08-06 20:24:51 -0400

    fix(scheduler)

--- src/server/src/services/puppeteer/PuppeteerService.ts
+++ src/server/src/services/puppeteer/PuppeteerService.ts
@@ -211,7 +211,7 @@ export class PuppeteerService extends BaseService {
       return `https:${url}`;
     } else
     if (/^\//.test(url)) {
-      return `https://${domain}${url}`;
+      return `https://${domain}/${url}`;
     } else
     if (/^\.\//.test(url)) {
       return `${targetUrl.replace(/\/+$/, '')}${url.replace(/^\./, '')}`;
