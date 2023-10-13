85ee7bbb2b41ad06f1987392c71197362b413f17
noodleofdeath <thom@noodleofdeath.com>
2023-10-13 14:34:40 -0400

    fix(RL-121): reduces memory footprint of widget

--- src/mobile/ios/Extensions/UIImage.swift
+++ src/mobile/ios/Extensions/UIImage.swift
@@ -6,20 +6,44 @@
 //
 
 import Foundation
+import ImageIO
 import UIKit
 
 extension UIImage {
   
-  func resized(toWidth width: CGFloat, isOpaque: Bool = true) -> UIImage? {
-    let size = CGSize(width: width, height: width / size.width * size.height)
-    return self.resized(toSize: size, isOpaque: isOpaque)
-  }
-  
-  func resized(toSize size: CGSize, isOpaque: Bool = true) -> UIImage? {
-    UIGraphicsBeginImageContextWithOptions(size, true, 1.0)
-    self.draw(in: CGRect(origin: .zero, size: size))
-    defer { UIGraphicsEndImageContext() }
-    return UIGraphicsGetImageFromCurrentImageContext()
+  static func load(from imageURL: URL, maxWidth: CGFloat) -> UIImage? {
+    
+    // Create an CGImageSource that represent an image
+    let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
+    guard let imageSource = CGImageSourceCreateWithURL(imageURL as CFURL, imageSourceOptions) else {
+      return nil
+    }
+    
+    guard let imageProperties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil) as Dictionary?,
+          let pixelWidth = imageProperties[kCGImagePropertyPixelWidth]?.floatValue,
+          let pixelHeight = imageProperties[kCGImagePropertyPixelHeight]?.floatValue
+    else {
+      return nil
+    }
+    
+    let size = CGSize(width: maxWidth, height: maxWidth / CGFloat(pixelWidth) * CGFloat(pixelHeight))
+    
+    // Calculate the desired dimension
+    let maxDimensionInPixels = max(size.width, size.height)
+    
+    // Perform downsampling
+    let options = [
+      kCGImageSourceCreateThumbnailFromImageAlways: true,
+      kCGImageSourceCreateThumbnailFromImageIfAbsent: true,
+      kCGImageSourceShouldCacheImmediately: true,
+      kCGImageSourceCreateThumbnailWithTransform: true,
+      kCGImageSourceThumbnailMaxPixelSize: maxDimensionInPixels
+    ] as CFDictionary
+    
+    guard let imgRef = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, options) else { return nil }
+    return UIImage(cgImage: imgRef)
   }
   
 }
+
+
