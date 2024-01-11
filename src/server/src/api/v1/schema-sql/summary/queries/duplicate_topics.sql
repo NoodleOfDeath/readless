bf8021f8aff650054516bd2a051d00786524aa16
NoodleOfDeath <thom@noodleofdeath.com>
2024-01-10 19:19:48 -0500

    RL-194:
    correct topic resolution

--- src/server/src/api/v1/schema-sql/summary/queries/duplicate_topics.sql
+++ src/server/src/api/v1/schema-sql/summary/queries/duplicate_topics.sql
@@ -1,11 +1,11 @@
 WITH DuplicateTopics AS (
   SELECT 
-    COUNT(ts."childId") AS count,
-    ARRAY_AGG(DISTINCT ts."groupId" ORDER BY ts."groupId" ASC) AS siblings
+    COUNT(DISTINCT ts."groupId") AS count,
+  ARRAY_AGG(DISTINCT ts."groupId" ORDER BY ts."groupId" ASC) AS siblings
   FROM topic_summaries ts
   GROUP BY ts."childId"
 )
-SELECT 
+SELECT
   siblings
 FROM DuplicateTopics
 WHERE
