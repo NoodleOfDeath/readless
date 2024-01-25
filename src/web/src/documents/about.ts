e3df6d0f4ef7eec514eac00c1561fb4341420519
NoodleOfDeath <thom@noodleofdeath.com>
2024-01-25 17:55:38 -0500

    web about

--- src/web/src/documents/about.ts
+++ src/web/src/documents/about.ts
@@ -2,15 +2,15 @@
 const data = `
 # About the Developer
 
-My name is Thom. I am a Full Stack Engineer living in Boston and originally from Northen Virginia. At the age of 16, I sustained a spinal cord injury, which in many ways has helped shape my success. This perspective has not only helped me find unique solutions to problems in my own life, but also in my work and personal projects.
+Ny name is Thom and I am a Full Stack Engineer based in Boston, originally from Northern Virginia. My journey hasn’t been typical. At the age of 16, I sustained a spinal cord injury that significantly shaped my life and career. This experience gave me a unique perspective, helping me find creative solutions in both my personal and professional challenge
 
-I have been presented the opportunity to work through very unique problems, that I realize I never would have as an able bodied person; in fact, less than 3% of the population experiences trying to go up an icy ramp using a wheelchair with groceries on their lap, which I must say, is quite an amusing physics challenge once one is in it.
+My foray into programming was as unconventional as it was impactful. My first program was a simple, yet useful, program on a TI-84 calculator written in C that assigned different sounds to each button, helping a visually impaired friend keep up with their studies. This project was my first step in realizing the power of technology to make a positive impact. 
 
-As I grew older, I wanted to create something unique that caters to underserved audiences and provides real value on a daily basis to users.
+During my high school years, I embarked on projects that combined robotics with practical problem-solving, such as designing robots capable of retrieving items from challenging locations. My collegiate journey deepened this exploration, leading me to delve into the intricacies of automotive technology. I learned to integrate nodes into a car's Controller Area Network (CAN), pioneering alternate vehicular control methods.
 
-In February 2023, I developed Read Less in a matter of weeks, shortly after the debut of the unofficial ChatGPT API node library and mainly for myself because in order to make change in the world, one must know what is happening in the world.
+As I matured, my aspirations evolved towards developing groundbreaking solutions for underrepresented communities, driven by a desire to create technology that offers tangible, daily value. This ambition culminated in February 2023 with the creation of 'Read Less'—a full stack application I developed within weeks end-to-end, inspired by the unveiling of the [unofficial ChatGPT API node library](https://www.npmjs.com/package/chatgpt), a project I helped maintain until the openai library was updated to include completions. Initially a tool for my personal enlightenment, 'Read Less' quickly became a testament to my belief that to effect change globally, one must be well-informed about worldly affairs.
 
-I can proudly say, I am more informed than I ever have been about current events, and this is a skill I hope to continue honing.
+Today, I take pride in being more knowledgeable about current events than ever before. This journey of self-improvement and dedication to meaningful technology is what drives me as an engineer.
 
 # App Development and Operations
 
@@ -28,11 +28,11 @@ Ideally, users will scroll through the app for only 15 minutes a day, then can f
 
 Plans for monetization include a subscription model for additional features (such as daily word games) in order to reach self-sustainability without heavy reliance on investors.
 
-So why reach out to OpenAI?  Well, with over 2,000 unique sign-ups - all achieved without marketing - it's clear that there is a demand for what we offer. 
+Wwith over 2,000 unique sign-ups - all achieved without marketing - it's clear that there is a demand for what we offer. 
 
 In fact, we are up against the founders of Instagram and their very similar platform called Artifact, which was released the same week as Read Less.
 
-Additionally, our journey mirrors that of OpenAI: starting with a noble mission and recognizing the need for sustainable growth to prevent potential missteps by others. 
+I believe our journey mirrors that of OpenAI: starting with a noble mission and recognizing the need for sustainable growth to prevent potential missteps by others. 
 
 With Read Less, I aspire to explore content discovery algorithms that, rather than echo viral or extreme views, foster open-mindedness and critical thinking, paving the way for a platform and brand that promotes this ethos. 
 `;
