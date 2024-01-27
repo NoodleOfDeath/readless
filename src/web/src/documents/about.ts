db4ed1c284c8b705e978a078e07c511308c7b718
NoodleOfDeath <thom@noodleofdeath.com>
2024-01-26 20:46:06 -0500

    web update

--- src/web/src/documents/about.ts
+++ src/web/src/documents/about.ts
@@ -2,7 +2,7 @@
 const data = `
 # App Development and Operations
 
-The app runs lean, with monthly infrastructure and API costs of around $450, soon to be cut in half when I switch to Mistral.
+Read Less backend runs lean, with monthly infrastructure and API costs of around $450, soon to be cut in half when we switch to Mistral.
 
 Tensorflow is run on CPU to resolve topics, and stable diffusion is used to generate images for articles without media.
 
@@ -16,12 +16,12 @@ Ideally, users will scroll through the app for only 15 minutes a day, then can f
 
 Plans for monetization include a subscription model for additional features (such as daily word games) in order to reach self-sustainability without heavy reliance on investors.
 
-Wwith over 2,000 unique sign-ups - all achieved without marketing - it's clear that there is a demand for what we offer. 
+With over 2,000 unique sign-ups - all achieved without marketing - it's clear that there is a demand for what we offer. 
 
 In fact, we are up against the founders of Instagram and their very similar platform called Artifact, which was released the same week as Read Less.
 
-I believe our journey mirrors that of OpenAI: starting with a noble mission and recognizing the need for sustainable growth to prevent potential missteps by others. 
+We believe our journey mirrors that of OpenAI: starting with a noble mission and recognizing the need for sustainable growth to prevent potential missteps by others. 
 
-With Read Less, I aspire to explore content discovery algorithms that, rather than echo viral or extreme views, foster open-mindedness and critical thinking, paving the way for a platform and brand that promotes this ethos. 
+With Read Less, we aspire to explore content discovery algorithms that, rather than echo viral or extreme views, foster open-mindedness and critical thinking, paving the way for a platform and brand that promotes this ethos. 
 `;
 export default data;
